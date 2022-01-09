770ea74ef97f9e3ddb1c51d9f13475ce52db45ba
Jared Kohrt <jkohrt7@gmail.com>
2022-01-08 21:50:18 -0500

    Initial commit

---
+++ README.old.md
@@ -0,0 +1,7 @@
+# friendforce
+
+## go team
+relationship crm tracker and coordinator
+
+## group members
+jared kohrt, jennifer liebelt, andrew bumgarner, alicia chen
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-red-800 font-semibold text-4xl">
+      Tailwind Test!
+    </div>
+  );
+}
+
+export default App;
