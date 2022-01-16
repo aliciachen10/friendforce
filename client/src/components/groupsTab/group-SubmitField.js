ee8ff0d40fe058eecb6f08a30791bbc64a8dd3a2
andrew bumgarner <andrewbumgarnerNC@gmail.com>
2022-01-15 22:07:08 -0800

    updated tabs to have editable fields

---
+++ client/src/components/groupsTab/group-SubmitField.js
@@ -0,0 +1,45 @@
+import { PencilAltIcon } from '@heroicons/react/solid';
+import { SaveAsIcon } from '@heroicons/react/solid';
+import React, {useState} from 'react';
+
+
+function SubmitGroupField (props) {
+
+    const [canUpdate, setUpdate] = useState(false)
+
+        const updatePalette = "";
+        const savedPallete = "";
+
+        function handleToUpdateClick(canUpdate) {
+            setUpdate(!canUpdate);
+        }
+
+        function handleToSavedClick(canUpdate) {
+
+
+            setUpdate(!canUpdate)
+        }
+
+
+        function swapUpdateModeButton(canUpdate) {
+            if(!canUpdate) {
+                return (<PencilAltIcon onClick = {() => handleToUpdateClick(canUpdate)} className="w-6 h-auto opacity-50 hover:cursor-pointer"/>)
+            }
+            else {
+                return (<SaveAsIcon onClick = {() => handleToSavedClick(canUpdate)} className= "w-6 h-auto opacity-50 hover:cursor-pointer"/>);
+            }
+
+        }
+    
+        return (
+            <div className = "flex flex-col">
+                <div>{props.name}</div>
+                <div className = {`flex justify-between bg-gray-100 border-gray-200 border-2 rounded-md p-1`}>
+                    <input className = "w-full bg-opacity-100" disabled = {!canUpdate} type = "text"></input>
+                    {swapUpdateModeButton(canUpdate)}
+                </div>
+            </div>
+        )
+}
+
+export default SubmitGroupField;
