ee4aee2ce568f0c17ed69df222d10a12a5adef5f
Jared Kohrt <jkohrt7@gmail.com>
2022-01-18 19:32:13 -0500

    clicking on a friend populates friendspage

--- client/src/components/friendsTab/friends-Data.js
+++ client/src/components/friendsTab/friends-Data.js
@@ -1,61 +1,54 @@
 import SubmitField from "./friends-SubmitField";
 import React, {useState} from "react";
 
-function FriendsData () {
+function FriendsData (props) {
+  //use selected friend prop's attributes instead.
+  let user = props.user;
 
-const [canUpdate, setUpdate] = useState(false);
-const [fullName, setName]          = useState("!")
-const [savedPhone, setPhone]       = useState("#"); 
-const [savedEmail, setEmail]       = useState("@");
-const [savedAddress, setAddress]   = useState("$");
-const [savedInterest, setInterest]       = useState(":)");
+  //TODO: we need to use user to get their groups and events.
 
+  return (
+      <div className = "flex flex-col"> 
+        <div>
+          <h3 className="text-lg leading-6 font-medium text-gray-900 text-center border-b-4 border-dotted border-indigo-300">Contact</h3>
+        </div>
+        <div className="bg-white shadow overflow-auto sm:rounded-md border-2 border-gray-200 m-4">
+          <dl className="divide-y divide-gray-200">
+            <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
+              <dt className="text-sm font-medium text-gray-500 pl-2 pt-2">Full name</dt>
+              <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2 pt-2 font-semibold">
+                  {/* <SubmitField stateSetter = {setName}/> */}
+                  {user.name}
+              </dd>
+            </div>
+            
+            <div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
+              <dt className="text-sm font-medium text-gray-500 pl-2 pt-2">Email address</dt>
+              <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2 pt-2 font-semibold">
+                {/* <SubmitField stateSetter = {setEmail}/> */}
+                {user.email}
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
+              <dt className="text-sm font-medium text-gray-500 pl-2 pt-2">Phone Number</dt>
+              <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2 pt-2 font-semibold">
+                {/* <SubmitField stateSetter = {setPhone}/> */}
+                {user.phone}
+              </dd>
+            </div>
+            <div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
+              <dt className="text-sm font-medium text-gray-500 pl-2 pt-2">Address</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 pt-2 font-semibold">
+                {/* <SubmitField stateSetter = {setAddress} /> */}
+                {user.address}
+              </dd>
+            </div>
 
-
-
-
-    return (
-       <div className = "grid"> 
-          <div>
-            <h3 className="text-lg leading-6 font-medium text-gray-900 text-center border-b-4 border-dotted border-indigo-300">Contact</h3>
-          </div>
-          <div className="bg-white shadow overflow-hidden sm:rounded-md border-2 border-gray-200 m-4">
-            <dl className="divide-y divide-gray-200">
-              <div className="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4">
-                <dt className="text-sm font-medium text-gray-500 pl-2 pt-2">Full name</dt>
-                <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                    <SubmitField stateSetter = {setName}/>
-                  
-                </dd>
-              </div>
-             
-              <div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
-                <dt className="text-sm font-medium text-gray-500 pl-2 pt-2">Email address</dt>
-                <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                  <SubmitField stateSetter = {setEmail}/>
-                  
-                </dd>
-              </div>
-              <div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
-                <dt className="text-sm font-medium text-gray-500 pl-2 pt-2">Phone Number</dt>
-                <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                  <SubmitField stateSetter = {setPhone}/>
-                  
-                </dd>
-              </div>
-              <div className="py-4 sm:grid sm:py-5 sm:grid-cols-3 sm:gap-4">
-                <dt className="text-sm font-medium text-gray-500 pl-2 pt-2">Address</dt>
-                <dd className="mt-1 flex text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                  <SubmitField stateSetter = {setAddress} />
-                 
-                </dd>
-              </div>
-
-            </dl>
-          </div>
-          </div> 
-        
-    )
+          </dl>
+        </div>
+        </div> 
+      
+  )
 
 
 
