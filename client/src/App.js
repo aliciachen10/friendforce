33820f7ef21b1f9feb1a4e5dc2ddde89af1071e6
jl118 <jen.liebelt@gmail.com>
2022-01-20 20:31:31 -0500

    fix: replace removed code from previous merge

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import './App.css';
 
 // universal components
