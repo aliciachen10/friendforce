891100910fb4d2519664816b505e3999069e6316
jl118 <jen.liebelt@gmail.com>
2022-01-24 21:30:13 -0500

    changed location of logout

--- client/src/components/Navbars/Navbar.js
+++ client/src/components/Navbars/Navbar.js
@@ -66,24 +66,7 @@ function Navbar(props) {
             </button>
 
             <div className="ml-3 relative">
-              <div>
-                <button type="button" className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button">
-                  <span className="sr-only">Open user menu</span>
-                  <img className="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""></img>
-                </button>
-              </div>
-
-                {/* Dropdown menu, show/hide based on menu state. */}
-
-                {/* Entering: "transition ease-out duration-100"
-                  From: "transform opacity-0 scale-95"
-                  To: "transform opacity-100 scale-100"
-                Leaving: "transition ease-in duration-75"
-                  From: "transform opacity-100 scale-100"
-                  To: "transform opacity-0 scale-95" */}
-              <div className="z-50 origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" tabIndex="-1">
-                <NavLink to="/" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-0" onClick={logout}>Log Out</NavLink>
-              </div>
+            <NavLink to="/" className={({isActive})=>(isActive ? selectedTabClass : unselectedTabClass )} onClick={logout}>Log Out</NavLink>
             </div>
           </div>
 
